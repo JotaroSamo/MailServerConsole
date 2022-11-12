ced53ee94a77c3e625a94a4fdec15fa977d533b8
Dimension-W <Dimension-W@HOME-PC>
2022-11-12 22:46:35 +0300

    up

--- MailServer/DB/Content/User.cs
+++ MailServer/DB/Content/User.cs
@@ -11,7 +11,7 @@ namespace Server.DB.Content
     {
         public int Id { get; set; }
 
-      
+     
         public string? Mail { get; set; }
         public string? Passowrd { get; set; }
     }
